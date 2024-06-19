42eaef753a796b30423cf45ee1fae02a0f2b18c7
Piyush-Aggarwal040501 <aggarwalpiyush85@gmail.com>
2024-06-19 20:18:49 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import { useState,useEffect } from 'react';
+import { BrowserRouter, Routes, Route,useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Enroll from './components/Enroll';
+
+import './App.css';
+
+function App() {
+  
+  
+  
+  return (
+    // <BrowserRouter>
+      
+    //   <Routes>
+    //     <Route path="/" element={<Login setUserData={setUserData} />} />
+    //     <Route path="/dashboard" element={<Home userData={userData} />} />
+    //     <Route path="/enroll" element={<Enroll userData={userData} setUserData={setUserData} />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>piyush is here</div>
+  );
+}
+
+export default App;
---
+++ client/src/info.js
@@ -0,0 +1,2 @@
+const server_base_url = "http://localhost:8000";
+export { server_base_url };
